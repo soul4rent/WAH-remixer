e712e995d36df1d273bfe2da0ec9a7e043ebc6bf
soul4rent <kyle.a.wadsworth@gmail.com>
2018-06-08 09:59:21 -0700

    what have I done?

--- Wah_Mixer.py
+++ Wah_Mixer.py
@@ -1,8 +1,18 @@
 from pygame import mixer
+import random
+import time
 #using pygame in case I want to add more features, like keystroke support or animations
 #might have to switch so I can run two pieces of beautiful music at the same time
 
 mixer.init()
-mixer.music.load("wah_sound.mp3")
-mixer.music.play()
 
+mixer.Channel(1).play(mixer.Sound('meme_music.wav')) #so it plays at the same time
+
+
+def WAH():
+    mixer.Channel(0).play(mixer.Sound('wah_sound.wav')) #its beautiful
+
+
+while True:
+    time.sleep(random.randint(0,10) * .1) #oh no
+    WAH()

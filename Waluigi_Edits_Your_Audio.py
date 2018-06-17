d3f62c4e9e18dfd9ffa7b38afbe315df65bbc671
soul4rent <kyle.a.wadsworth@gmail.com>
2018-06-17 11:25:50 -0700

    Its more beautiful than I could possibly imagine: now to automate uploading!

--- Waluigi_Edits_Your_Audio.py
+++ Waluigi_Edits_Your_Audio.py
@@ -1,23 +1,28 @@
 from pydub import AudioSegment
+import random
 
 
+def WahDividers(sndLength):
+    return sndLength - random.randint(0,sndLength) 
+
+wahCount = 100 #number of wahs
 sound = AudioSegment.from_file("meme_music.wav")
+wahSound = AudioSegment.from_file("wah_sound.wav") #a beautiful cry of a beautiful perrson
+wahPoints = []
 
-halfway_point = len(sound) // 2
-first_half = sound[:halfway_point]
-second_half = sound[halfway_point:]
 
-# create a new file "first_half.mp3":
-first_half.export("Waluigi Automatic Edits/first_half.wav", format="wav")
-second_half.export("Waluigi Automatic Edits/second_half.wav", format="wav")
+for x in range(wahCount):
+    wahPoints.append(WahDividers(len(sound)))
+wahPoints = sorted(wahPoints) #points where waluigi annoyingly yells "wah"
 
-sound1 = AudioSegment.from_wav("Waluigi Automatic Edits/first_half.wav")
-sound2 = AudioSegment.from_wav("Waluigi Automatic Edits/second_half.wav")
-WAH = AudioSegment.from_wav("wah_sound.wav")
 
-combined_sounds = sound1 + WAH + sound2
-#For now, Waluigi will "WAH" in the middle of your audio.
-#TODO: Random Waluigi edits.
+print (wahPoints)
+print(len(sound))
 
-combined_sounds.export("path.wav", format="wav")
+completeSound = sound
+for x in range(0, len(wahPoints)):
+    completeSound = completeSound.overlay(wahSound, position=wahPoints[x])
+
+
+completeSound.export("beauty.wav", format="wav")
 

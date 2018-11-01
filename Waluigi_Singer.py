c8ae8784ad39f1acb166c9ba80c76f0ecb17c075
soul4rent <kyle.a.wadsworth@gmail.com>
2018-11-01 16:24:38 -0700

    refactoring start for flexible, future use

---
+++ Waluigi_Singer.py
@@ -0,0 +1,13 @@
+import pytumblr
+from pytube import YouTube
+from pydub import AudioSegment
+import praw
+import os
+import subprocess
+import random
+import secure
+
+class Waluigi_Singer:
+    #Underconstruction
+    def __init__(self):
+        print("Created Successfully")
